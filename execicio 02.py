220df284d953ebd63850e2227f179d7f99bb9bff
Antonio Luiz M Ramos Jr <rewris@gmail.com>
2023-07-23 13:40:45 -0300

    adicionei a operação de divisão

--- execicio 02.py
+++ execicio 02.py
@@ -3,10 +3,13 @@ numero2 = int(input('Digite outro número: '))
 soma = numero1 + numero2
 multiplicacao = numero1 * numero2
 subtracao = numero1 - numero2
-opcao = int(input('1 - Soma\n2 - Multiplicacao \n3 - Subtração\nEscolha uma opção: '))
+divisao = numero1 / numero2
+opcao = int(input('1 - Soma\n2 - Multiplicacao \n3 - Subtração\n4 - Divisão \nEscolha uma opção: '))
 if opcao == 1:
 	print(f'A soma entre os números é {soma}')
 if opcao == 2:
 	print(f'A multiplicacao entre os números é {Multiplicacao}')
 if opcao == 3:
 	print(f'A Subtração entre os números é  {subtracao}')
+if opcao == 4:
+    print(f'A Divisão entre os números é {divisao}')
